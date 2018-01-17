8c062b70440d1ebe936d9a3fb56f9ad46f024c22
liuwei <lwei_jlu@foxmail.com>
2018-01-17 22:09:49 +0800

    ubuntuUpdate

--- src/page/index/Recommend.vue
+++ src/page/index/Recommend.vue
@@ -1,15 +1,10 @@
 <template>
   <div class="maindiv">
     <swiper :options="swiperOption" ref="mySwiper">
-      <!-- <swiper-slide v-for="item,index in banner" v-bind:key="index">
+      <swiper-slide v-for="item,index in banner" v-bind:key="index">
         <img v-bind:src="item.pic">
         <div class="footdiv" v-bind:style="{ background: item.titleColor }">{{ item.typeTitle }}</div>
-      </swiper-slide> -->
-      <!-- <swiper-slide>1</swiper-slide>
-      <swiper-slide>12</swiper-slide>
-      <swiper-slide>14</swiper-slide>
-      <swiper-slide>15</swiper-slide>
-      <swiper-slide>15</swiper-slide> -->
+      </swiper-slide>
     </swiper>
     <div>
       <button>私人FM</button>
@@ -17,13 +12,21 @@
       <button>歌单</button>
       <button>排行榜</button>
     </div>
-    <div>
+    <!-- <div>
      <icon name="flag"></icon>
      <icon name="music"></icon>
      <icon name="signal" flip="horizontal"></icon>
 
+    </div> -->
+    <div class="privatecontent">
+      <div style="position: absolute;top:0;left:0;">独家放送></div>
+      <ul>
+        <li v-for="item,index in privatecontent" v-bind:key="index" style="width:50%;">
+          <img :src="item.sPicUrl" style="zoom:0.5">
+          <p>{{item.name}}</p>
+        </li>
+      </ul>  
     </div>
-
     
   </div>
 </template>
@@ -54,27 +57,32 @@ export default {
     },
     getResource () { // 推荐歌单
       this.$http.get(apiurl + '/recommend/resource').then(res => {
-        this.resource = res.data.banners
+        this.resource = res.data.result
       })
     },
     getPrivatecontent () { // 独家放送
       this.$http.get(apiurl + '/personalized/privatecontent').then(res => {
-        this.privatecontent = res.data.banners
+        this.privatecontent = res.data.result
+        console.log(this.privatecontent.length)
       })
     },
     getNewsong () { // 推荐新音乐
       this.$http.get(apiurl + '/personalized/newsong').then(res => {
-        this.privatecontent = res.data.banners
+        this.newsong = res.data.result
       })
     }
   },
   created () {
-    // this.getBanner()
+    this.getBanner()
+   // this.getResource()
+    this.getPrivatecontent()
+    this.getNewsong()
   }
 }
 </script>
 
 <style>
+
   .maindiv{
     position: relative;
     height: 200px;
@@ -88,5 +96,17 @@ export default {
     padding-left: 15px;
     padding-right: 10px;
     color: #FFF;
+  }
+  .privatecontent{
+    position:absolute;
+    margin-top: 50px;
+  }
+  ul{
+    padding: 0px;
+    margin: 0px;
+  }
+  li{
+    list-style-type: none;
+    float: left;
   }
 </style>
